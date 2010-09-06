83832b8bdd47fa3fcd24c168d0c6e26d3998141f
David Almeida Pitanguy <kalibukalibu@gmail.com>
2010-09-06 15:49:48 -0400

    Atualização do projeto. Adicionado embaralhamento.

--- src/games/puzzle/PuzzleCanvas.java
+++ src/games/puzzle/PuzzleCanvas.java
@@ -1,5 +1,7 @@
 package games.puzzle;
 
+import java.util.Random;
+
 import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
@@ -7,30 +9,57 @@ import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.midlet.MIDlet;
 
+/**
+ * @author David Almeida Pitanguy
+ * @date 06/09/2010
+ */
 public class PuzzleCanvas extends Canvas implements CommandListener {
+
+	/**
+	 * Quantidade de movimentos a fazer para embaralhar
+	 */
+	private static final int VEZES_EMBARALHAR = 1000;
+	
+	/**
+	 * Quantidade de pedras por coluna
+	 */
+	private static final int PEDRAS_POR_COLUNA = 4;
+	
+	private static final String TITULO = "15-Puzzle";
 
 	private MIDlet midlet;
 	private Command sair;
 	private int[][] pecas = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 },
 			{ 9, 10, 11, 12 }, { 13, 14, 15, 0 } };
+
 	private int posicaoDoZeroX = 3;
 	private int posicaoDoZeroY = 3;
 
 	private int ppX;
 	private int ppY;
-	private int margem = getWidth() < getHeight() ? getWidth() / 4
-			: getHeight() / 4;
+	private int margem = getWidth() < getHeight() ? getWidth()
+			/ PEDRAS_POR_COLUNA : getHeight() / PEDRAS_POR_COLUNA;
 
+	/**
+	 * Construtor responsavel por fazer todo o necessario para inicializar o jogo.
+	 * 
+	 * @param midlet
+	 */
 	public PuzzleCanvas(MIDlet midlet) {
-		setTitle("Jogo Maluco");
+		setTitle(TITULO);
 		this.midlet = midlet;
 
 		sair = new Command("Sair", Command.EXIT, 1);
 
 		addCommand(sair);
 		setCommandListener(this);
+		
+		embaralhar();
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.Canvas#paint(javax.microedition.lcdui.Graphics)
+	 */
 	protected void paint(Graphics g) {
 
 		int alturaPeca = getHeight() / 4;
@@ -60,78 +89,143 @@ public class PuzzleCanvas extends Canvas implements CommandListener {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
+	 */
 	public void commandAction(Command c, Displayable d) {
 		if (c == this.sair) {
 			midlet.notifyDestroyed();
 		}
 	}
 
-	protected void keyPressed(int keyCode) {
-		int tecla = getGameAction(keyCode);
-
-		if (tecla == Canvas.UP) {
+	/**
+	 * Altera a posição das peças caso for possivel.
+	 * 
+	 * @param codeGameAction
+	 */
+	private void alterarPosicaoPecas(int codeGameAction) {
+		switch (codeGameAction) {
+		case (Canvas.UP): {
 			if (posicaoDoZeroX + 1 < 4) {
 				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX + 1][posicaoDoZeroY];
 				posicaoDoZeroX++;
 			}
+			break;
 		}
-		if (tecla == Canvas.DOWN) {
+		case (Canvas.DOWN): {
 			if (posicaoDoZeroX - 1 > -1) {
 				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX - 1][posicaoDoZeroY];
 				posicaoDoZeroX--;
 			}
+			break;
 		}
-		if (tecla == Canvas.LEFT) {
+		case (Canvas.LEFT): {
 			if (posicaoDoZeroY + 1 < 4) {
 				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX][posicaoDoZeroY + 1];
 				posicaoDoZeroY++;
 			}
+			break;
 		}
-		if (tecla == Canvas.RIGHT) {
+		case (Canvas.RIGHT): {
 			if (posicaoDoZeroY - 1 > -1) {
 				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX][posicaoDoZeroY - 1];
 				posicaoDoZeroY--;
 			}
+			break;
 		}
+		}
+
 		pecas[posicaoDoZeroX][posicaoDoZeroY] = 0;
 
 		repaint();
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.Canvas#keyPressed(int)
+	 */
+	protected void keyPressed(int keyCode) {
+		alterarPosicaoPecas(getGameAction(keyCode));
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.Canvas#pointerReleased(int, int)
+	 */
+	protected void pointerReleased(int x, int y) {
+		if ((ppY - y) > margem) {
+			alterarPosicaoPecas(Canvas.UP);
+		}
+		if ((y - ppY) > margem) {
+			alterarPosicaoPecas(Canvas.DOWN);
+		}
+		if ((ppX - x) > margem) {
+			alterarPosicaoPecas(Canvas.LEFT);
+		}
+		if ((x - ppX) > margem) {
+			alterarPosicaoPecas(Canvas.RIGHT);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.Canvas#pointerPressed(int, int)
+	 */
 	protected void pointerPressed(int x, int y) {
 		this.ppX = x;
 		this.ppY = y;
 	}
 
-	protected void pointerReleased(int x, int y) {
-		if ((ppY - y) > margem) {
-			if (posicaoDoZeroX + 1 < 4) {
-				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX + 1][posicaoDoZeroY];
-				posicaoDoZeroX++;
-			}
-		}
-		if ((y - ppY) > margem) {
-			if (posicaoDoZeroX - 1 > -1) {
-				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX - 1][posicaoDoZeroY];
-				posicaoDoZeroX--;
-			}
-		}
-		if ((ppX - x) > margem) {
-			if (posicaoDoZeroY + 1 < 4) {
-				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX][posicaoDoZeroY + 1];
-				posicaoDoZeroY++;
-			}
-		}
-		if ((x - ppX) > margem) {
-			if (posicaoDoZeroY - 1 > -1) {
-				pecas[posicaoDoZeroX][posicaoDoZeroY] = pecas[posicaoDoZeroX][posicaoDoZeroY - 1];
-				posicaoDoZeroY--;
-			}
+	/**
+	 * Embalha as peças.
+	 */
+	private void embaralhar() {
+
+		int atual = -1;
+		int ultimo = -1;
+		int qtdUltimo = 0;
+		Random r = new Random();
+
+		for (int i = 0; i < VEZES_EMBARALHAR; i++) {
+
+			do {
+				atual = r.nextInt(PEDRAS_POR_COLUNA);
+
+				if (atual == ultimo) {
+					qtdUltimo++;
+				}else{
+					qtdUltimo = 0;
+				}
+			} while (qtdUltimo >= 4);
+
+			colocaValoresCorretosEmbaralhar(atual);
+
+			ultimo = atual;
 		}
 
-		pecas[posicaoDoZeroX][posicaoDoZeroY] = 0;
+	}
 
-		repaint();
+	/**
+	 * Utilizado para transformar os comandos em codigos do celular.
+	 * 
+	 * @param code
+	 */
+	private void colocaValoresCorretosEmbaralhar(int code) {
+		switch (code) {
+		case 0: {
+			alterarPosicaoPecas(Canvas.UP);
+			break;
+		}
+		case 1: {
+			alterarPosicaoPecas(Canvas.DOWN);
+			break;
+		}
+		case 2: {
+			alterarPosicaoPecas(Canvas.LEFT);
+			break;
+		}
+		case 3: {
+			alterarPosicaoPecas(Canvas.RIGHT);
+			break;
+		}
+		}
 	}
 
 }
--- src/games/puzzle/PuzzleMIDlet.java
+++ src/games/puzzle/PuzzleMIDlet.java
@@ -5,22 +5,38 @@ import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
+/**
+ * @author David Almeida Pitanguy
+ * @date 06/09/2010
+ */
 public class PuzzleMIDlet extends MIDlet {
 
 	private Canvas canvas;
 
+	/**
+	 * Construtor responsavel por definir a tela que ira aparecer.
+	 */
 	public PuzzleMIDlet() {
 		canvas = new PuzzleCanvas(this);
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#destroyApp(boolean)
+	 */
 	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#pauseApp()
+	 */
 	protected void pauseApp() {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#startApp()
+	 */
 	protected void startApp() throws MIDletStateChangeException {
 		Display.getDisplay(this).setCurrent(canvas);
 	}
